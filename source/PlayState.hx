0fd27bfd2e1bbe2c19c001a2083462b2d94cf1d7
padvk <patrick.havard@hotmail.com>
2015-01-06 07:49:24 +0000

    Moved our movement function to the PlayState.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -9,6 +9,7 @@ import flixel.FlxState;
 import flixel.group.FlxTypedGroup;
 import flixel.text.FlxText;
 import flixel.tile.FlxTilemap;
+import flixel.tweens.FlxTween;
 import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
 import flixel.util.FlxRect;
@@ -22,6 +23,8 @@ class PlayState extends FlxState {
 	private var _mTiles:FlxTilemap;
 	private var _tileSize:Float = 16.0;
 	private var _grpBombs:FlxTypedGroup<Bomb>;
+	private var _playerMoving:Bool = false;
+	private var _moveTime:Float = .25;
 	
 	/**
 	 * Function that is called up when to state is created to set it up. 
@@ -60,10 +63,11 @@ class PlayState extends FlxState {
 
 	override public function update():Void {
 		super.update();
+		playerMovement();
 		FlxG.collide(_grpBombs, _mTiles);
 		FlxG.collide(_grpBombs, _grpBombs);
 		
-		if (FlxG.keys.anyJustPressed(["E"])) {
+		if (FlxG.keys.anyJustPressed(["E", "SPACE"])) {
 			placeBomb();
 		}
 	}
@@ -97,7 +101,7 @@ class PlayState extends FlxState {
 		}
 		x = indexCopy * _tileSize;
 		var y:Float = Math.floor(index / _mTiles.widthInTiles) * _tileSize; //NOT WORKING RIGHT NOW
-		_player = new Player(_mTiles, x, y);
+		_player = new Player(x, y);
 		
 		//make tile + surrounding tiles walkable in case they where made breakable
 		if (_mTiles.getTileByIndex(index) == 3) {
@@ -125,5 +129,69 @@ class PlayState extends FlxState {
 			_mTiles.setTileByIndex(tileIsFloor[index], 3, true);
 			tileIsFloor.remove(tileIsFloor[index]);
 		}
+	}
+	
+	private function playerMovement():Void {
+		var _up:Bool = false;
+		var _down:Bool = false;
+		var _left:Bool = false;
+		var _right:Bool = false;
+		 
+		_up = FlxG.keys.anyPressed(["UP", "W"]);
+		_down = FlxG.keys.anyPressed(["DOWN", "S"]);
+		_left = FlxG.keys.anyPressed(["LEFT", "A"]);
+		_right = FlxG.keys.anyPressed(["RIGHT", "D"]);
+		
+		//Setting direction of tween and facing
+		if (_playerMoving == false && (_up || _down || _left || _right)) {
+			//Finding coordinates of tile we need to check
+			var checkTileX:Int = 0;
+			var checkTileY:Int = 0;
+			var checkTile:Int;
+			if (_up) {
+				checkTileX = Math.round(_player.x/16);
+				checkTileY = Math.round((_player.y - 16)/16);
+			} else if (_down) {
+				checkTileX = Math.round(_player.x/16);
+				checkTileY = Math.round((_player.y + 16)/16);
+			} else if (_left) {
+				checkTileX = Math.round((_player.x - 16)/16);
+				checkTileY = Math.round(_player.y/16);
+			} else if (_right) {
+				checkTileX = Math.round((_player.x + 16)/16);
+				checkTileY = Math.round(_player.y/16);
+			}
+			checkTile = _mTiles.getTile(checkTileX, checkTileY);
+			if (checkTile == 1) {
+				_playerMoving = true;
+				if (_up) {
+					FlxTween.tween(_player, { y:_player.y - 16 }, _moveTime, { complete:endTween });
+					_player.facing = FlxObject.UP;
+				} else if (_down) {
+					FlxTween.tween(_player, { y:_player.y + 16 }, _moveTime, { complete:endTween });
+					_player.facing = FlxObject.DOWN;
+				} else if (_left) {
+					FlxTween.tween(_player, { x:_player.x - 16 }, _moveTime, { complete:endTween });
+					_player.facing = FlxObject.LEFT;
+				} else if (_right) {
+					FlxTween.tween(_player, { x:_player.x + 16 }, _moveTime, { complete:endTween });
+					_player.facing = FlxObject.RIGHT;
+				}
+				
+				//Play animations
+				switch(_player.facing) {
+					case FlxObject.LEFT, FlxObject.RIGHT:
+						_player.animation.play("lr");
+					case FlxObject.UP:
+						_player.animation.play("u");
+					case FlxObject.DOWN:
+						_player.animation.play("d");
+				}
+			}
+		}
+	}
+	
+	private function endTween(T:FlxTween):Void {
+		_playerMoving = false;
 	}
 }
--- source/Player.hx
+++ source/Player.hx
@@ -14,12 +14,9 @@ import flixel.util.FlxMath;
  * @author ...
  */
 class Player extends FlxSprite {
-	private var _moveTime:Float = .25;
-	private var _moving:Bool = false;
 	public var bombs:Int = 0;
-	private var TILES:FlxTilemap;
 
-	public function new(objTILES:FlxTilemap, X:Float=0, Y:Float=0) {
+	public function new(X:Float=0, Y:Float=0) {
 		super(X, Y);
 		loadGraphic(AssetPaths.player__png, true, 16, 16);
 		setFacingFlip(FlxObject.LEFT, false, false);
@@ -27,75 +24,9 @@ class Player extends FlxSprite {
 		animation.add("lr", [3, 4, 3, 5], 6, false);
 		animation.add("u", [6, 7, 6, 8], 6, false);
 		animation.add("d", [0, 1, 0, 2], 6, false);
-		TILES = objTILES;
-	}
-	
-	private function movement():Void {
-		var _up:Bool = false;
-		var _down:Bool = false;
-		var _left:Bool = false;
-		var _right:Bool = false;
-		 
-		_up = FlxG.keys.anyPressed(["UP", "W"]);
-		_down = FlxG.keys.anyPressed(["DOWN", "S"]);
-		_left = FlxG.keys.anyPressed(["LEFT", "A"]);
-		_right = FlxG.keys.anyPressed(["RIGHT", "D"]);
-		
-		//Setting direction of tween and facing
-		if (_moving == false && (_up || _down || _left || _right)) {
-			//Finding coordinates of tile we need to check
-			var checkTileX:Int = 0;
-			var checkTileY:Int = 0;
-			var checkTile:Int;
-			if (_up) {
-				checkTileX = Math.round(x/16);
-				checkTileY = Math.round((y - 16)/16);
-			} else if (_down) {
-				checkTileX = Math.round(x/16);
-				checkTileY = Math.round((y + 16)/16);
-			} else if (_left) {
-				checkTileX = Math.round((x - 16)/16);
-				checkTileY = Math.round(y/16);
-			} else if (_right) {
-				checkTileX = Math.round((x + 16)/16);
-				checkTileY = Math.round(y/16);
-			}
-			checkTile = TILES.getTile(checkTileX, checkTileY);
-			if (checkTile == 1) {
-				_moving = true;
-				if (_up) {
-					FlxTween.tween(this, { y:y - 16 }, _moveTime, { complete:endTween });
-					facing = FlxObject.UP;
-				} else if (_down) {
-					FlxTween.tween(this, { y:y + 16 }, _moveTime, { complete:endTween });
-					facing = FlxObject.DOWN;
-				} else if (_left) {
-					FlxTween.tween(this, { x:x - 16 }, _moveTime, { complete:endTween });
-					facing = FlxObject.LEFT;
-				} else if (_right) {
-					FlxTween.tween(this, { x:x + 16 }, _moveTime, { complete:endTween });
-					facing = FlxObject.RIGHT;
-				}
-				
-				//Play animations
-				switch(facing) {
-					case FlxObject.LEFT, FlxObject.RIGHT:
-						animation.play("lr");
-					case FlxObject.UP:
-						animation.play("u");
-					case FlxObject.DOWN:
-						animation.play("d");
-				}
-			}
-		}
-	}
-	
-	private function endTween(T:FlxTween):Void {
-		_moving = false;
 	}
 	
 	override public function update():Void {
-		movement();
 		super.update();
 	}
 }
